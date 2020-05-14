3a972e3d166581510c22797977f548dbef04d416
robehind <574544947@qq.com>
2020-05-14 17:37:19 +0800

    gcn args

--- exp_args/gcn_args.py
+++ exp_args/gcn_args.py
@@ -2,7 +2,7 @@ from .default_args import args
 
 args.update(
     train_scenes = {'bathroom':[31],},
-    train_targets = {'bathroom':["Sink","ToiletPaper","Towel","Cloth"],},
+    train_targets = {'bathroom':["Sink"],},
     test_scenes = {'bathroom':[31],},
     test_targets = {'bathroom':["Sink","ToiletPaper","Towel","Cloth"],},
     action_dict = {
@@ -14,12 +14,12 @@ args.update(
     },
     state_dict = {
         'fc|4':'resnet50_fc.hdf5',
-        'RGB':'images.hdf5'
+        'score':'resnet50_score.hdf5'
         },
     grid_size = 0.5,
-    total_train_epi = 20000,
+    total_train_epi = 4000,
     total_eval_epi = 1000,
-    threads = 1,
+    threads = 4,
     log_title = 'ScenePriors',
     optimizer = 'Adam',
     model = 'ScenePriorsModel',
@@ -27,9 +27,9 @@ args.update(
     optim_args = dict(lr = 0.0007,),
     print_freq = 100,
     max_epi_length = 100,
-    model_save_freq = 10000,
+    model_save_freq = 4000,
     nsteps = 50,
-    verbose = True,
+    verbose = False,
     gpu_ids = -1,
     #load_model_dir = "../check_points/A3C_20000_2020-05-07_16-40-59.dat",
     results_json = "result_demo.json"
