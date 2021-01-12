895e1acfb44e824f19b5c5f503d962b217630266
robehind <574544947@qq.com>
2021-01-12 18:12:00 +0800

    exp args file

--- exp_args/brain_exp/CNNpretrain.py
+++ exp_args/brain_exp/CNNpretrain.py
@@ -1,6 +1,7 @@
 from ..default_args import args
 
 args.update(
+    exps_dir = '../brain_exp/resnet50lstm',
     train_scenes = {
         'kitchen':'1-15',
         'living_room':'1-15',
@@ -17,10 +18,22 @@ args.update(
         'bedroom':["HousePlant", "Lamp", "Book", "AlarmClock"],
         'bathroom':["Sink", "ToiletPaper", "SoapBottle", "LightSwitch"],
     },
-    #train_scenes = {'kitchen':'25'},
-    #train_targets = {'kitchen':["Microwave", 'Sink'],},
-    test_scenes = {'kitchen':'25',},
-    test_targets = {'kitchen':["Microwave", 'Sink'],},
+    test_scenes = {
+        'kitchen':'16-20',
+        'living_room':'16-20',
+        'bedroom':'16-20',
+        'bathroom':'16-20',
+    },
+    test_targets = {
+        'kitchen':[
+            "Toaster", "Microwave", "Fridge","CoffeeMaker",
+            ],
+        'living_room':[
+            "Pillow", "Laptop", "Television","GarbageCan",
+            ],
+        'bedroom':["HousePlant", "Lamp", "Book", "AlarmClock"],
+        'bathroom':["Sink", "ToiletPaper", "SoapBottle", "LightSwitch"],
+    },
     action_dict = {
         'MoveAhead':['m0'],
         'TurnLeft':['r-45'],
@@ -28,7 +41,7 @@ args.update(
         'Done':None,
     },
     obs_dict = {
-        'fc':'rgbpred_fc.hdf5',
+        'fc':'resnet50_fc_new.hdf5',
     },
     target_dict = {
         'glove':'../thordata/word_embedding/word_embedding.hdf5',
@@ -37,20 +50,29 @@ args.update(
     rotate_angle = 45,
     total_train_frames = 1e8,
     total_eval_epi = 1000,
-    threads = 1,
-    exp_name = 'CNNpretrainLstm',
+    threads = 16,
+    exp_name = 'basic1',
     optimizer = 'RMSprop',
     model = 'FcLstmModel',
     agent = 'A2CLstmAgent',
     runner = 'A2CRunner',
-    loss_func = 'loss_with_entro',
+    loss_func = 'basic_loss',
     trainer = 'a2c_train',
     optim_args = dict(lr = 0.0007,alpha = 0.99, eps = 0.1),
     print_freq = 10000,
     max_epi_length = 200,
     model_save_freq = 1e7,
-    nsteps = 10,
+    nsteps = 40,
     gpu_ids = [0],
+    reward_dict = {
+        'collision':-0.1,
+        'step':-0.01,
+        'SuccessDone':10,
+        'FalseDone':0,
+        #'angle_restrict': -0.05
+    },
+    #load_model_dir = '/home/zhiyu/brain_exp/resnet50lstm/basic3/FcLstmModel_100000000_173532.dat',
+    #load_optim_dir = '../EXPS/CNNpretrainLstm_Stable_5R_210109_202939/optim/RMSprop_100000000_021719.dat'
 )
 model_args_dict = dict(
         action_sz = len(args.action_dict)
